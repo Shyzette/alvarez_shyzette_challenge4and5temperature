76f8334e5aeeab5b2583c08e29daa7b77094106e
Shyzette <72762164+Shyzette@users.noreply.github.com>
2020-11-24 11:20:34 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-if (input.temperature(TemperatureUnit.Fahrenheit) > 80) {
+if (input.temperature(TemperatureUnit.Fahrenheit) < 80) {
     light.setAll(light.rgb(255, 0, 0))
-} else if (input.temperature(TemperatureUnit.Fahrenheit) < 40) {
+} else if (input.temperature(TemperatureUnit.Fahrenheit) > 40) {
     light.setAll(light.rgb(0, 255, 0))
 } else {
     light.setAll(light.rgb(0, 0, 255))
